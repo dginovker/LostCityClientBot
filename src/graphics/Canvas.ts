f86c676ad4032d142b78aea09b5e85df9acfb699
Pazaz <pazaz@itspazaz.com>
2026-01-29 17:39:06 -0500

    feat: Optimize PixMap drawing

--- src/graphics/Canvas.ts
+++ src/graphics/Canvas.ts
@@ -1,9 +1,14 @@
 export const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
-export const canvas2d: CanvasRenderingContext2D = canvas?.getContext('2d', { willReadFrequently: true })!;
+export const canvas2d: CanvasRenderingContext2D = canvas?.getContext('2d', {
+    desynchronized: false,
+    alpha: false
+})!;
 
 export const jpegCanvas: HTMLCanvasElement = document.createElement('canvas');
 export const jpegImg: HTMLImageElement = document.createElement('img');
-export const jpeg2d: CanvasRenderingContext2D = jpegCanvas.getContext('2d', { willReadFrequently: true })!;
+export const jpeg2d: CanvasRenderingContext2D = jpegCanvas.getContext('2d', {
+    willReadFrequently: true
+})!;
 
 export function saveDataURL(dataURL: string, filename: string) {
     const link = document.createElement('a');
